965b9bf6b27e57fcf5ba99b713a5ffe8fea12671
Johanna <jespoof@outlook.com>
2020-11-01 00:09:27 +0200

    riippuvuuksien injektointi osa 4

--- viikko2/tehtavat8-12/Verkkokauppa1/src/main/java/ohtu/Main.java
+++ viikko2/tehtavat8-12/Verkkokauppa1/src/main/java/ohtu/Main.java
@@ -3,15 +3,17 @@ package ohtu;
 import ohtu.verkkokauppa.*;
 
 public class Main {
+    
+    
 
     public static void main(String[] args) {
-        Kauppa kauppa = new Kauppa(
-            Varasto.getInstance(), 
-            Pankki.getInstance(), 
-            Viitegeneraattori.getInstance() 
-        );
+        Viitegeneraattori viitegen = new Viitegeneraattori();
+        Kirjanpito kirjanpito      = new Kirjanpito();
+        Varasto varasto            = new Varasto(kirjanpito);
+        Pankki pankki              = new Pankki(kirjanpito);
+        Kauppa kauppa              = new Kauppa(varasto, pankki, viitegen);
 
-        // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
+         // kauppa hoitaa yhden asiakkaan kerrallaan seuraavaan tapaan:
         kauppa.aloitaAsiointi();
         kauppa.lisaaKoriin(1);
         kauppa.lisaaKoriin(3);
@@ -28,7 +30,7 @@ public class Main {
         kauppa.tilimaksu("Arto Vihavainen", "3425-1652");
 
         // kirjanpito
-        for (String tapahtuma : Kirjanpito.getInstance().getTapahtumat()) {
+        for (String tapahtuma : kirjanpito.getTapahtumat()) {
             System.out.println(tapahtuma);
         }
     }
